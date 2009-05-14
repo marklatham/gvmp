a25d641cbf4eb821c947e62042c623ecdf10ef38
Greg Moreno <greg.moreno@gmail.com>
2009-05-13 22:50:18 -0700

    Added time format.

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,4 +1,7 @@
-ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
+date_time_formats = {
   :text_date => "%B %d, %Y",
   :Ymd       => "%Y-%m-%d" 
-)
+}
+
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(date_time_formats)
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(date_time_formats)
