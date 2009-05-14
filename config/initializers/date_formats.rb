bf9ff4bd37175cef0b54e5f252d5635e75a19806
Greg Moreno <greg.moreno@gmail.com>
2009-05-13 22:10:28 -0700

    Move date/time intializer code.

---
+++ config/initializers/date_formats.rb
@@ -0,0 +1,4 @@
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
+  :text_date => "%B %d, %Y",
+  :Ymd       => "%Y-%m-%d" 
+)
---
+++ config/initializers/fixtures.rb
@@ -0,0 +1,2 @@
+ENV['FIXTURES_PATH'] = File.join('spec', 'fixtures')
+
