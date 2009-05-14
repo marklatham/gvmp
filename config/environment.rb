605863d8ee299b5b16bfafc23bd2ecd07d2f4b69
Greg Moreno <greg.moreno@gmail.com>
2009-05-13 23:51:22 -0700

    Started using haml. see communities/index.html.haml

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,10 @@ RAILS_GEM_VERSION = '2.3.2'  unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+
+
+  config.gem 'haml', :version => '= 2.0.9'
+
   # Make Time.zone default to the specified zone, and make Active Record store time values
   # in the database in UTC, and return them converted to the specified local zone.
   # Run "rake -D time" for a list of tasks for finding time zone names. Uncomment to use default local time.
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090510234012) do
+ActiveRecord::Schema.define(:version => 20090505051246) do
 
   create_table "colleagues", :force => true do |t|
     t.string   "given_name"
