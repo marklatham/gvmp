b62a764c2398f36d9d4973b45a74a902060e73a0
Mark Latham <mark@votermedia.org>
2009-05-24 09:47:35 -0700

    Controllers converted to resource_controller.

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -1,86 +1,14 @@
 class FaqsController < ApplicationController
-  # GET /faqs
-  # GET /faqs.xml
-  def index
+  resource_controller
+
+  create.flash  "Faq successfully created."
+  update.flash  "Faq was successfully updated."
+  destroy.flash "Faq removed."
+  
+  index.before do
     @faqs = Faq.find(:all, :order => "sequence")
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @faqs }
-      format.yaml  { render :yaml => @faqs }
-    end
   end
+  
+  index.wants.yaml { render :yaml => collection }
 
-  # GET /faqs/1
-  # GET /faqs/1.xml
-  def show
-    @faq = Faq.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @faq }
-    end
-  end
-
-  # GET /faqs/new
-  # GET /faqs/new.xml
-  def new
-    @faq = Faq.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @faq }
-    end
-  end
-
-  # GET /faqs/1/edit
-  def edit
-    @faq = Faq.find(params[:id])
-  end
-
-  # POST /faqs
-  # POST /faqs.xml
-  def create
-    @faq = Faq.new(params[:faq])
-
-    respond_to do |format|
-      if @faq.save
-        flash[:notice] = 'Faq was successfully created.'
-        format.html { redirect_to(faqs_url) }
-        format.xml  { render :xml => @faq, :status => :created, :location => @faq }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @faq.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /faqs/1
-  # PUT /faqs/1.xml
-  def update
-    @faq = Faq.find(params[:id])
-
-    respond_to do |format|
-      if @faq.update_attributes(params[:faq])
-        flash[:notice] = 'Faq was successfully updated.'
-        format.html { redirect_to(faqs_url) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @faq.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /faqs/1
-  # DELETE /faqs/1.xml
-  def destroy
-    @faq = Faq.find(params[:id])
-    @faq.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(faqs_url) }
-      format.xml  { head :ok }
-    end
-  end
 end
