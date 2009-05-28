aef48c91342e205061ae734ee8a3fe10d9e96437
Mark Latham <mark@votermedia.org>
2009-05-28 14:48:54 -0700

    Restored code in websites/new that I omitted May 20. Redirect after add website. Some admin data management.

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -3,12 +3,25 @@ class RankingsController < ApplicationController
   before_filter :redirect_unless_admin_user
 
   def index
-    @rankings = Ranking.find(:all, :order => "id")
+    @rankings = Ranking.find(:all, :order => "created_at DESC")
  
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @rankings }
       format.yaml  { render :yaml => @rankings }
+    end
+  end
+
+
+  # DELETE /rankings/1
+  # DELETE /rankings/1.xml
+  def destroy
+    @ranking = Ranking.find(params[:id])
+    @ranking.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(rankings_url) }
+      format.xml  { head :ok }
     end
   end
 
--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -3,11 +3,21 @@ class WebsitesController < ApplicationController
 
   before_filter :redirect_unless_admin_user, :except => [:new, :create]
 
+  index.before do
+    @websites = Website.find(:all, :order => "id DESC")
+  end
+  
+  index.wants.yaml { render :yaml => collection }
+
   new_action.before do
     @community = Community.find(params[:community_id])
   end
 
-  index.wants.yaml { render :yaml => collection }
+  create.before do
+    @community = Community.find(params[:community_id])
+  end
+  
+  create.wants.html { redirect_to :controller => 'communities', :action => 'show', :id => @community.id }
 
   private
 
