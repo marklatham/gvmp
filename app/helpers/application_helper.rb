86a0058b00e9a984653bf5ece4354547022da806
Greg Moreno <greg.moreno@gmail.com>
2009-06-27 15:55:36 -0700

    Minor header cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,12 @@ module ApplicationHelper
     string.include?("://") and 
     string.include?(".")
   end
+
   def link_if_url(string)
     link_to_if is_url?(string), string, string, :target => "_blank"
+  end
+
+  def link_separator(s = '|')
+    content_tag(:span, s, :class => 'separator')
   end
 end
