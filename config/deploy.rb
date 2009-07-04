23d79eaab5d246acd1376b8142aa313385fc530f
Greg Moreno <greg.moreno@gmail.com>
2009-07-03 17:45:41 -0700

    That should fix sphinx.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,9 +4,6 @@ set :user , "votermed"
 # The domain name of the server to deploy to, this can be your domain or the domain of the server.
 set :server_name, "votermedia.org"
 
-# Your repository type, by default we use subversion. 
-#set :scm, :subversion
-
 # If you are using git, uncomment the following line and comment out the line above.
 set :scm, :git
 
@@ -18,7 +15,6 @@ set :application, "gvmp"
 set :repository,  "git://git.assembla.com/gvmp.git"
 
 
-###### There is no need to edit anything below this line ######
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :use_sudo, false
 set :group_writable, false
@@ -28,9 +24,10 @@ role :app, server_name
 role :web, server_name
 role :db,  server_name, :primary => true
 
-# set the proper permission of the public folder
-task :after_update_code, :roles => [:web, :db, :app] do
+task :after_update_code, :roles => :app do
   db.symlink
+  sphinx.symlink 
+  sphinx.configure
 end
 
 namespace :deploy do
@@ -46,6 +43,7 @@ namespace :passenger do
 
   desc "Restart dispatchers"
   task :restart do
+    sphinx.restart
     run "touch #{current_path}/tmp/restart.txt"
   end
 end
@@ -54,7 +52,31 @@ namespace :db do
 
   task :symlink do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config"
-#    run "ln -nfs #{shared_path}/config/production.sphinx.conf #{release_path}/config"
+  end
+
+end
+
+namespace :sphinx do 
+  task :symlink, :roles => :app do
+    run "ln -nfs #{shared_path}/config/sphinx.yml #{release_path}/config"
+    run "ln -nfs #{shared_path}/db/sphinx #{release_path}/db/"
+  end
+
+  task :configure, :roles => :app do
+    run "cd #{release_path} && rake thinking_sphinx:configure RAILS_ENV=production"
+  end
+
+  task :start, :roles => :app do
+    run "cd #{current_path} && rake thinking_sphinx:start RAILS_ENV=production"
+  end
+
+  task :stop , :roles => :app do
+    run "cd #{current_path} && rake thinking_sphinx:stop RAILS_ENV=production"
+  end
+
+  task :restart, :roles => :app do
+    stop
+    start
   end
 
 end
