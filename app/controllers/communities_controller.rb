419d29c6ede2b6ef90da249e12a49bfdf8c455cc
Mark Latham <mark@votermedia.org>
2009-08-06 11:02:42 -0700

    Percent vote tallying: first complete algorithm code. Output available on debug pages only, not yet on community pages for user viewing.

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -130,32 +130,200 @@ class CommunitiesController < ApplicationController
   end
   
   def debug
+  # Should skip this if community has no websites/rankings or no votes, else probably get error.
+  # TO DO: any vote for a share that's not a whole number percent should be split,
+  # e.g. a vote for 26.4% should become 0.4 of a vote for 27% and 0.6 of a vote for 26%.
+  # This algorithm doesn't handle tie votes "fairly": it gives all the tied share to the first website.
+  # So should probably be enhanced some time to make it fairer. But this problem is not significant when there are many
+  # votes and some are over 10 days old.
+  
+    days_full_value = 10
+    days_valid = 60
+    ranking_formula_denominator = 50
+    
     @community = Community.find(params[:id])
+    
+    # How to select the latest vote from each IP address for each website?:
 
-# This doesn't get all data fields, so no good I guess:
-#    @votes = Vote.maximum(:updated_at, 
-#                               :conditions => ["community_id = ?", @community.id],                     
-#                               :group => "ip_address, website_id")    
+    # This doesn't get all data fields, so no good I guess:
+    #    @votes = Vote.maximum(:updated_at, 
+    #                               :conditions => ["community_id = ?", @community.id],                     
+    #                               :group => "ip_address, website_id")    
 
-# Found example at http://stackoverflow.com/questions/1129792/rails-active-record-find-in-conjunction-with-order-and-group but...
-# Unsure how to code with just one query, so used naive way with too many queries:
+    # Found example at http://stackoverflow.com/questions/1129792/rails-active-record-find-in-conjunction-with-order-and-group but...
+    # Unsure how to code with just one query, so used naive way with too many queries:
 
-# This almost works, but returns the first record of each group (by ID, regardless of how sorted) while we want the latest record:
-    @votes = Vote.find(:all, :conditions => ["community_id = ?", @community.id], :order => "website_id, ip_address, updated_at DESC", :group => "website_id, ip_address")    
-# So this finds last record in each group:
+    # This almost works, but returns the first record of each group (by ID, regardless of how sorted) while we want the latest record:
+    @votes = Vote.find(:all, :conditions => ["community_id = ?", @community.id], :order => "website_id, ip_address, created_at DESC", :group => "website_id, ip_address")    
+    # So this finds last record in each group (but even this doesn't work!):
     @votes.each do |vote|
-      temp = Vote.find(:last, :conditions => ["community_id = ? and website_id = ? and ip_address = ?", @community.id, vote.website_id, vote.ip_address], :order => "website_id, ip_address, updated_at")
-      vote.updated_at = temp.updated_at
+      temp = Vote.find(:last, :conditions => ["community_id = ? and website_id = ? and ip_address = ?", @community.id, vote.website_id, vote.ip_address], :order => "created_at")
+      vote.created_at = temp.created_at
       vote.support = temp.support
     end
     
     @rankings = Ranking.find(:all, :conditions => ["community_id = ?", @community.id], :order => "website_id")
 
-# Make sure shares are whole numbers adding to 100 (not coded yet):
-    sum = 0.0
+    # Make sure shares are nonegative whole numbers, not all zero:
     @rankings.each do |ranking|
-#      ranking.share = 0.0
+      ranking.share = ranking.share.round
+      if ranking.share < 0.0
+        ranking.share = 0.0
+      end
+      ranking.save
     end
+    if @rankings.sum(&:share) <= 0.0
+      @rankings.each do |ranking|
+        ranking.share = 1.0
+        ranking.save
+      end
+    end
+
+    # Calculate count0 (# votes for share or more) and count1 (# votes for share+1 or more) for each ranking:
+    # (Should probably be put into a method to reduce repetitive coding below.)
+    @rankings.each do |ranking|
+      ranking.count0 = 0.0
+      ranking.count1 = 0.0
+      @votes.each do |vote|
+        if vote.website_id == ranking.website_id && vote.support
+          
+          if vote.support >= ranking.share + 1.0
+            days_old = (Time.now.to_date - vote.created_at.to_date).to_i
+            if days_old < days_full_value
+              ranking.count1 += 1.0
+              ranking.count0 += 1.0
+            elsif days_old < days_valid
+              ranking.count1 += (days_valid - days_old) / ranking_formula_denominator.to_f
+              ranking.count0 += (days_valid - days_old) / ranking_formula_denominator.to_f
+            end
+            
+          elsif vote.support >= ranking.share
+            days_old = (Time.now.to_date - vote.created_at.to_date).to_i
+            if days_old < days_full_value
+              ranking.count0 += 1.0
+            elsif days_old < days_valid
+              ranking.count0 += (days_valid - days_old) / ranking_formula_denominator.to_f
+            end
+          
+          end
+        end
+      end
+      ranking.save
+    end
+    
+    # Need to adjust shares until they sum to 100.0 and max_count1 <= min_count0(for websites with positive shares).
+    # @max_count1 is the ranking record for the website that most deserves to have its share increased.
+    # @min_count0 is the ranking record for the website that most deserves to have its share decreased.
+    
+    @max_count1 = @rankings.max {|a,b| a.count1 <=> b.count1 }
+    # Can only reduce a share if it's positive, so:
+    @rankings_pos = @rankings.find_all {|r| r.share > 0.0 }
+    @min_count0 = @rankings_pos.min {|a,b| a.count0 <=> b.count0 }
+    # but if all shares = 0.0, @min_count0 is nil which gives error; hence prevented above.
+    
+    # If shares sum to more than 100 (which shouldn't happen, but just in case), decrease 1 at a time:
+    while @rankings.sum(&:share) > 100.0
+      @min_count0.share -= 1.0
+      @min_count0.count1 = @min_count0.count0
+      @min_count0.count0 = 0.0
+      @votes.each do |vote|
+        if vote.website_id == @min_count0.website_id && vote.support
+          if vote.support >= @min_count0.share
+            days_old = (Time.now.to_date - vote.created_at.to_date).to_i
+            if days_old < days_full_value
+              @min_count0.count0 += 1.0
+            elsif days_old < days_valid
+              @min_count0.count0 += (days_valid - days_old) / ranking_formula_denominator.to_f
+            end
+          end
+        end
+      end
+      @min_count0.save
+      
+      @rankings = Ranking.find(:all, :conditions => ["community_id = ?", @community.id], :order => "website_id")
+      @max_count1 = @rankings.max {|a,b| a.count1 <=> b.count1 }
+      @rankings_pos = @rankings.find_all {|r| r.share > 0.0 }
+      @min_count0 = @rankings_pos.min {|a,b| a.count0 <=> b.count0 }
+    end
+    
+    # If shares sum to less than 100 (e.g. when first website[s] added), increase 1 at a time:
+    while @rankings.sum(&:share) < 100.0
+      @max_count1.share += 1.0
+      @max_count1.count0 = @max_count1.count1
+      @max_count1.count1 = 0.0
+      @votes.each do |vote|
+        if vote.website_id == @max_count1.website_id && vote.support
+          if vote.support >= @max_count1.share + 1.0
+            days_old = (Time.now.to_date - vote.created_at.to_date).to_i
+            if days_old < days_full_value
+              @max_count1.count1 += 1.0
+            elsif days_old < days_valid
+              @max_count1.count1 += (days_valid - days_old) / ranking_formula_denominator.to_f
+            end
+          end
+        end
+      end
+      @max_count1.save
+      
+      @rankings = Ranking.find(:all, :conditions => ["community_id = ?", @community.id], :order => "website_id")
+      @max_count1 = @rankings.max {|a,b| a.count1 <=> b.count1 }
+      @rankings_pos = @rankings.find_all {|r| r.share > 0.0 }
+      @min_count0 = @rankings_pos.min {|a,b| a.count0 <=> b.count0 }
+    end
+
+    # Main loop: Adjust shares until max_count1 <= min_count0 i.e. find a cutoff number of votes (actually a range of cutoffs)
+    # where shares sum to 100.0 using that same cutoff to determine each website's share:
+    
+    # Can change "while" to "if" and watch the iterations by refreshing /communities/debug/nn
+    while @min_count0.count0 < @max_count1.count1
+      # Move one percent share from @min_count0 to @max_count1
+      
+      @min_count0.share -= 1.0
+      @min_count0.count1 = @min_count0.count0
+      @min_count0.count0 = 0.0
+      @votes.each do |vote|
+        if vote.website_id == @min_count0.website_id && vote.support
+          if vote.support >= @min_count0.share
+            days_old = (Time.now.to_date - vote.created_at.to_date).to_i
+            if days_old < days_full_value
+              @min_count0.count0 += 1.0
+            elsif days_old < days_valid
+              @min_count0.count0 += (days_valid - days_old) / ranking_formula_denominator.to_f
+            end
+          end
+        end
+      end
+      @min_count0.save
+      
+      @max_count1.share += 1.0
+      @max_count1.count0 = @max_count1.count1
+      @max_count1.count1 = 0.0
+      @votes.each do |vote|
+        if vote.website_id == @max_count1.website_id && vote.support
+          if vote.support >= @max_count1.share + 1.0
+            days_old = (Time.now.to_date - vote.created_at.to_date).to_i
+            if days_old < days_full_value
+              @max_count1.count1 += 1.0
+            elsif days_old < days_valid
+              @max_count1.count1 += (days_valid - days_old) / ranking_formula_denominator.to_f
+            end
+          end
+        end
+      end
+      @max_count1.save
+      
+      @rankings = Ranking.find(:all, :conditions => ["community_id = ?", @community.id], :order => "website_id")
+      @rankings_pos = @rankings.find_all {|r| r.share > 0.0 }
+      @min_count0 = @rankings_pos.min {|a,b| a.count0 <=> b.count0 }
+      @max_count1 = @rankings.max {|a,b| a.count1 <=> b.count1 }
+    end
+
+    # If needed: Code to initialize all shares to 0.0:
+    #    @rankings = Ranking.find(:all)
+    #    @rankings.each do |ranking|
+    #      ranking.share = 0.0
+    #      ranking.save
+    #    end
 
     respond_to do |format|
       format.html # debug.html.erb
