c1ced714c96d71b6018474147b7d17573453e1c4
Tim Morgan <tmorgan99@gmail.com>
2010-01-06 23:31:01 -0800

    fix bug in community/show and remove vendor/gems

--- db/schema.rb
+++ db/schema.rb
@@ -213,9 +213,9 @@ ActiveRecord::Schema.define(:version => 20100103230052) do
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
   create_table "votes", :force => true do |t|
-    t.string   "ip_address"
     t.integer  "community_id"
     t.integer  "website_id"
+    t.string   "ip_address"
     t.float    "support"
     t.datetime "created_at"
     t.datetime "updated_at"
