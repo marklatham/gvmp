5f4d739409b17102c53c1a794fe78f0d0c32e2db
Tim Morgan <tmorgan99@gmail.com>
2010-01-12 16:08:05 -0800

    gems back!

---
+++ vendor/gems/loofah-0.4.1/lib/loofah/html/document.rb
@@ -0,0 +1,19 @@
+module Loofah
+  module HTML # :nodoc:
+    #
+    #  Subclass of Nokogiri::HTML::Document.
+    #
+    #  See Loofah::InstanceMethods for additional methods.
+    #
+    class Document < Nokogiri::HTML::Document
+      include Loofah::InstanceMethods
+
+      private
+
+      def sanitize_roots # :nodoc:
+        xpath("/html/head","/html/body")
+      end
+
+    end
+  end
+end
