9bb0b1389e606cab5c67e04f5bd9f5f63d626dfc
Mark Latham <mark@votermedia.org>
2010-01-16 16:32:35 -0800

    Respond appropriately once a funding is 100% allocated: After that, a 0% share is OK but a positive share is not OK.

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -10,16 +10,19 @@ class Ranking < ActiveRecord::Base
                               :rank => self.rank, :ranking_updated_at => self.updated_at, :share => self.share,
                               :status => self.status, :count0 => self.count0, :count1 => self.count1})
     
-    @fundings = Funding.find(:all, :conditions => ["date = ? AND allocated < ?", Date.today, 100])
-    if @fundings
+    if @fundings = Funding.find(:all, :conditions => ["date = ? AND community_id = ?", Date.today, pr.community_id])
       @fundings.each do |funding|
-        if funding.community_id == pr.community_id
+        
+        if funding.allocated + pr.share <= 100.0
           funding.allocated += pr.share
           pr.funds += funding.amount
           pr.award += funding.amount * pr.share / 100.0
           funding.save
           pr.save
+        else
+          puts "Tried to allocate over 100% of funding ID " + funding.id.to_s
         end
+        
       end
     end
     
