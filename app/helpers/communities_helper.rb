83eb8392763bd3c843104dcc880b91f7b49e0840
Greg Moreno <greg.moreno@gmail.com>
2010-01-19 19:17:28 -0800

    Use methods.

--- app/helpers/communities_helper.rb
+++ app/helpers/communities_helper.rb
@@ -4,21 +4,27 @@ module CommunitiesHelper
   end
 
   def render_category(community)
-    community.category
+    h community.category
   end
 
-  def render_location(community)
-    location = [:city, :prov_state, :country].inject([]) { |l, f| l << community.send(f) }
-    location.reject{|i| i.blank?}.join(', ')
+  def render_location(community, area=nil)
+    if area.nil?
+      location = [:city, :prov_state, :country].inject([]) { |l, f| l << community.send(f) }
+      location.reject{|i| i.blank?}.join(', ')
+    else
+      h truncate(community.send(area), :length => 15)
+    end
   end
 
   def render_scope(community, length = 20)
     h truncate(community.scope, length)
   end
 
-  def render_websites_info(community)
+  def render_websites_count(community)
+    # TODO: Cache the websites count
     number_with_delimiter(community.websites.count)
   end
+
 
   def render_community_actions(community)
     return unless current_user
