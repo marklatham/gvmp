b65eb1f927f5d33a47c7f68893d8d45030bfd4b8
Mark Latham <mark@votermedia.org>
2010-05-30 16:57:26 -0700

    FAQ layout improvements.

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -9,10 +9,6 @@ class FaqsController < ApplicationController
   update.flash  "Faq was successfully updated."
   destroy.flash "Faq removed."
   
-  index.before do
-    @faqs = Faq.find(:all, :order => "sequence")
-  end
-  
   index.wants.yaml { render :yaml => collection }
 
 end
