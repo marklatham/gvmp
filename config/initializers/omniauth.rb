5e2396b1c15cf72aaa4901591098554ab97d9f4f
Mark Latham <mark@votermedia.org>
2011-04-01 10:19:48 -0700

    Store authentication API secrets in database instead of in open source code. Minor content touchups.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '179591418728914', '[insert manually for now]'
-  provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-  provider :twitter, 'Q7ewc5XJe2yWYt6VCueImA', '[insert manually for now]'
+  
+  facebook_secret = Authentication.where("user_id = ? and provider = ?", -1, "facebook").first.uid
+  provider :facebook, '179591418728914', facebook_secret
+  
+  linked_in_secret = Authentication.where("user_id = ? and provider = ?", -1, "linked_in").first.uid
+  provider :linked_in, '7nDh7oX2EyciTyguIrLvoaU2ASKLK5gjkxC179piy40eiRK7iHRq-TOdu1vBIXnz', linked_in_secret
+  
+  twitter_secret = Authentication.where("user_id = ? and provider = ?", -1, "twitter").first.uid
+  provider :twitter, 'Q7ewc5XJe2yWYt6VCueImA', twitter_secret
+  
 end
