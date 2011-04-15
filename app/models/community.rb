4b27a0538ba55f325f32ec5016916246713c7596
Mark Latham <mark@votermedia.org>
2011-04-14 18:56:37 -0700

    Misc content & layout edits.

--- app/models/community.rb
+++ app/models/community.rb
@@ -536,7 +536,7 @@ class Community < ActiveRecord::Base
     # TODO: A community should be marked by admin as featured
     # Should be a named_scope eventually
     def featured
-      ids = [82, 96, 94, 51, 52, 81, 5, 205, 59, 3, 151, 116, 109, 223]
+      ids = [82, 96, 175, 51, 52, 81, 5, 205, 59, 3, 151, 116, 109, 223]
       find(:all, :conditions => ["id in (?)", ids]).sort{|a, b| ids.index(a.id) <=> ids.index(b.id)}
     end
   end
