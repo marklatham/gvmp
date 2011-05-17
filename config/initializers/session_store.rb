e6808631c7dea5cb8f0b274834df508dff366dd2
Mark Latham <mark@votermedia.org>
2011-05-17 15:54:13 -0700

    Fix CookieOverflow bug.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Gvmp::Application.config.session_store :cookie_store, :key => '_gvmp_session'
+# Was getting error ActionDispatch::Cookies::CookieOverflow so advice from
+# blog.anands.net/2010/06/30/migrating-a-rails-app-from-rails-2-3-8-to-rails-3-0-0-beta-4/ :
+
+# Gvmp::Application.config.session_store :cookie_store, :key => '_gvmp_session'
+Rails.application.config.session_store :active_record_store, :key => '_gvmp_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
