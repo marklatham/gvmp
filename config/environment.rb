e10598c4141949de2c19c0a4ebf92b418a592bbc
Mark Latham <mark@votermedia.org>
2011-08-30 16:06:55 -0700

    Quick fix for SSL verification bug.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Quick fix for a bug -- see comment in https://github.com/indirect/jquery-rails/issues/56
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
 # Initialize the rails application
 Gvmp::Application.initialize!
