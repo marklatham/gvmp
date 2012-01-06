c742260209ce24d0fbd8dce9c77d694973e763d7
Mark Latham <mark@votermedia.org>
2012-01-05 16:34:06 -0800

    UBC AMS contest changes.

--- config/routes.rb
+++ config/routes.rb
@@ -33,27 +33,29 @@ Gvmp::Application.routes.draw do
   resources :votes
   resources :websites
 
-  match '/admin',               :to => 'admin#admin',           :as => 'admin'
-  match '/t',                   :to => 'admin#terminal',        :as => 'terminal'
-  match '/shared_ips',          :to => 'admin#shared_ips',      :as => 'shared_ips'
-  match '/gpw',                 :to => 'clippings#gpw',         :as => 'gpw'
-  match '/:idstring/horserace', :to => 'communities#horserace', :as => 'horserace'
-  match '/horserace/:id',       :to => 'communities#horserace', :as => 'horserace_old'
-  match '/:idstring/votes',     :to => 'communities#votes',     :as => 'community_votes'
-  match '/mlresume',            :to => 'minimal#mlresume',      :as => 'mlresume'
-  match '/temp',                :to => 'minimal#temp',          :as => 'temp'  # Temporary use, not part of website.
-  match '/about_contact',       :to => 'public#about_contact',  :as => 'about_contact'
-  match '/bcmuni',              :to => 'public#bcmuni',         :as => 'bcmuni'
-  match '/comdump.yaml',        :to => 'public#comdump',        :as => 'comdump'
-  match '/terms',               :to => 'public#terms',          :as => 'terms'
-  match '/privacy',             :to => 'public#privacy',        :as => 'privacy'
-  match '/how_you_can_help',    :to => 'public#how_you_can_help', :as => 'how_you_can_help'
-  match '/ubc2010',             :to => 'public#ubc2010',        :as => 'ubc2010'
-  match '/ubcentry',            :to => 'public#ubcentry',       :as => 'ubcentry'
-  match '/publications',        :to => 'pubs#index',             :as => 'publications'
-  match '/votesummary',         :to => 'votes#summary',         :as => 'votesummary'
+  match '/admin',               :to => 'admin#admin',                 :as => 'admin'
+  match '/t',                   :to => 'admin#terminal',              :as => 'terminal'
+  match '/shared_ips',          :to => 'admin#shared_ips',            :as => 'shared_ips'
+  match '/gpw',                 :to => 'clippings#gpw',               :as => 'gpw'
+  match '/:idstring/horserace', :to => 'communities#horserace',       :as => 'horserace'
+  match '/horserace/:id',       :to => 'communities#horserace',       :as => 'horserace_old'
+  match '/:idstring/votes',     :to => 'communities#votes',           :as => 'community_votes'
+  match '/mlresume',            :to => 'minimal#mlresume',            :as => 'mlresume'
+  match '/temp',                :to => 'minimal#temp',                :as => 'temp'  # Temporary use, not part of website.
+  match '/about_contact',       :to => 'public#about_contact',        :as => 'about_contact'
+  match '/bcmuni',              :to => 'public#bcmuni',               :as => 'bcmuni'
+  match '/comdump.yaml',        :to => 'public#comdump',              :as => 'comdump'
+  match '/terms',               :to => 'public#terms',                :as => 'terms'
+  match '/privacy',             :to => 'public#privacy',              :as => 'privacy'
+  match '/how_you_can_help',    :to => 'public#how_you_can_help',     :as => 'how_you_can_help'
+  match '/ubc2010',             :to => 'public#ubc2010',              :as => 'ubc2010'
+  match '/ubc2012',             :to => 'public#ubc2012',              :as => 'ubc2012'
+  match '/ubcentry',            :to => 'public#ubc2012',              :as => 'ubc2012'
+  match '/ubc1vm',              :to => 'public#ubc1vm',               :as => 'ubc1vm'
+  match '/publications',        :to => 'pubs#index',                  :as => 'publications'
+  match '/votesummary',         :to => 'votes#summary',               :as => 'votesummary'
   
-   match '/:idstring',           :to => 'communities#show',      :as => 'idstring'
+   match '/:idstring',           :to => 'communities#show',           :as => 'idstring'
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
